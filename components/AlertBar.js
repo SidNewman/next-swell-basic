83a625e5e0a5e8a1b6f153ccae369ad1bbb57142
Sid Newman <104797880+SidNewman@users.noreply.github.com>
2023-02-22 23:03:53 +0000

    :sparkles: + alert bar component

---
+++ components/AlertBar.js
@@ -0,0 +1,9 @@
+import styles from './Header.module.scss'
+
+const AlertBar = () => {
+    return (
+        <div className={styles.alert}>Free shipping on orders over £50</div>
+    )
+}
+
+export default AlertBar
